bbd001b0df344811f0363ffab543460282030058
dabratte <abrattediego@gmail.com>
2018-09-23 17:22:48 -0300

    acctualizo main

--- src/Python/cgmii_sim.py
+++ src/Python/cgmii_sim.py
@@ -25,139 +25,20 @@ def main():
 
 	for clock in range (0,NCLOCK): # MAIN LOOP
 		
-		raw_block = cgmii_module.tx_raw #bloque recibido desde cgmii
-		raw_block = list_to_hex(raw_block)
 		
+		tx_raw = cgmii_module.tx_raw #bloque recibido desde cgmii
 		#codificacion
-		if raw_block in encoder : 
-			coded_block = tx.encoder[raw_block]
+		if tx_raw['block_name'] in tx.ENCODER : 
+			tx_coded = tx.ENCODER[ tx_raw['block_name'] ]
 		else :
-			coded_block = CODED_ERROR_BLOCK
+			tx_block = tx.ENCODER['ERROR_BLOCK']
 
-		coded_vector.append(coded_block) #solo para debugging
+		coded_vector.append(tx_coded) #solo para debugging
+		
 		cgmii_module.change_state(0)
 
+	bp()
 
-
-<<<<<<< HEAD
-		if 
-
-	bp() 
-
-=======
-<<<<<<< HEAD
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
-				self.state_sequence.append(self.variables) # LOGEO
-
-			elif self.state == TX_C:
-				self.variables[0] = state_name[self.state] # LOGEO
-
-				########funcionalidad#########
-				
-				if self.idle_counter < NIDLE:
-					self.tx_raw = IDLE_BLOCK
-					self.idle_counter+=1
-					self.state = TX_C
-				else :
-					self.tx_raw = START_BLOCK
-					self.state = TX_D
-					self.data_counter+=1
-				#############################
-				
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
-				self.state_sequence.append(self.variables) # LOGEO
-				
-	
-			elif self.state == TX_D:
-				self.variables[0] = state_name[self.state] # LOGEO
-
-				######funcionalidad###########
-				if self.data_counter < NDATA:
-					self.tx_raw = DATA_BLOCK
-					self.state = TX_D
-					self.data_counter+=1
-				else:
-					self.tx_raw = T0_BLOCK
-					self.state = TX_T
-				#############################
-
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
-				self.state_sequence.append(self.variables) # LOGEO
-				
-
-			elif self.state == TX_T:
-				self.variables[0] = state_name[self.state] # LOGEO
-
-				#####funcionalidad############
-				self.tx_raw = IDLE_BLOCK
-				self.state = TX_C
-				self.idle_counter = 0
-				self.data_counter = 0
-				##############################
-
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
-				self.state_sequence.append(self.variables) # LOGEO
-
-				
-			elif self.state == TX_E:
-				self.variables[0] = state_name[self.state] # LOGEO
-
-				####funcionalidad#######
-				self.tx_raw = ERR_BLOCK
-				self.state = TX_E
-				########################			         
-
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
-				self.state_sequence.append(self.variables) # LOGEO
-
-class Scrambler(object) :
-	def __init__(self):
-		self.BLOCK_CODED_LEN = 64
-		self.SCRAMBLER_LEN = 58
-		self.polynom_1 = 38
-		self.polynom_2 = 57
-		self.shift_reg = map(int,np.zeros(58))
-		self.output = 0x000000000000000000
-		self.xor = 0	
-	def tx_scrambling(self,in_block):
-		#bypass de los dos bit de sh
-		self.output |= ( in_block & (1 << 65) )
-		self.output |= ( in_block & (1 << 64) )
-		#realizo scrambling
-		'''
-			OJO !!!! DEBERIA INVERTIR EL ORDEN DE LOS OCTETOS creo
-		''' 
-		for i in reversed(range(0,self.BLOCK_CODED_LEN)):
-			self.xor = ( (in_block & (1<<i) ) >>i ) ^ self.shift_reg[self.polynom_1] ^ self.shift_reg[self.polynom_2]
-			self.shift_reg = np.roll(self.shift_reg,1)
-			self.output |= self.xor << i
-			self.shift_reg[0] = self.xor
-			
-		return self.output
-	def rx_scrambling(self,in_block):
-		#bypass de los dos bit de sh
-		self.output |= ( in_block & (1 << 65) )
-		self.output |= ( in_block & (1 << 64) )
-		#realizo scrambling
-		'''
-			OJO !!!! DEBERIA INVERTIR EL ORDEN DE LOS OCTETOS creo
-		''' 
-		for i in reversed(range(0,self.BLOCK_CODED_LEN)):
-			self.xor = ( (in_block & (1<<i) ) >>i ) ^ self.shift_reg[self.polynom_1] ^ self.shift_reg[self.polynom_2]
-			self.shift_reg = np.roll(self.shift_reg,1)
-			self.output |= self.xor << i
-			self.shift_reg[0] = ( (in_block & (1<<i) ) >>i )
-			
-		return self.output		
-		
-=======
->>>>>>> cce3534754ac640885eaf054854ac1cee08a259f
->>>>>>> ff02858db82e78befed4abbc2a383569e3c517bc
 
 	
 
