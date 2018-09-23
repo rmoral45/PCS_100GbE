e86e003b14268b9aed528b2d12c896a32fad78b2
dabratte <abrattediego@gmail.com>
2018-09-23 16:13:46 -0300

    main modificado

--- src/Python/cgmii_sim.py
+++ src/Python/cgmii_sim.py
@@ -3,40 +3,45 @@
 import random
 import numpy as np
 from pdb import set_trace as bp
-from tx_modules import *
-
+import tx_modules as tx 
 NCLOCK = 40
 NIDLE = 5
 NDATA = 16
+NLANES = 20
 
 
 
 
 
 def main():
-	bp()
+	#------------------------init--------------------#
+	tx_fifo = []
 	coded_vector = []
+	lanes = [[] for y in range(NLANES)]
 
-	cgmii_module = CgmiiFSM()
-	tx_scrambler_module = Scrambler()
+	cgmii_module = tx.CgmiiFSM()
+	tx_scrambler_module = tx.Scrambler()
 	#rx_scrambler = Scrambler()
 
 	for clock in range (0,NCLOCK): # MAIN LOOP
-		#bloque recibido desde cgmii
-		raw_block = cgmii_module.tx_raw
+		
+		raw_block = cgmii_module.tx_raw #bloque recibido desde cgmii
 		raw_block = list_to_hex(raw_block)
 		
 		#codificacion
 		if raw_block in encoder : 
-			coded_block = encoder[raw_block]
+			coded_block = tx.encoder[raw_block]
 		else :
 			coded_block = CODED_ERROR_BLOCK
 
-		coded_vector.append(coded_block)
+		coded_vector.append(coded_block) #solo para debugging
 		cgmii_module.change_state(0)
 
-	bp()
 
+
+		if 
+
+	bp() 
 
 
 	
