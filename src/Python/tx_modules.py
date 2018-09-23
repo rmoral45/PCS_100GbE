cce3534754ac640885eaf054854ac1cee08a259f
dabratte <abrattediego@gmail.com>
2018-09-23 16:10:37 -0300

    agregado de funciones

--- src/Python/tx_modules.py
+++ src/Python/tx_modules.py
@@ -1,23 +1,115 @@
 
 import random
 import numpy as np
+from pdb import set_trace as bp
 
+
+
+################# VARIABLES ####################
+NIDLE = 5
+NDATA = 16
+NLANES = 20
+
+################################################
+
+def parallel_conversion(tx_fifo,lanes):
+	"""Funcionalidad de conversor de paralelismo 1:N
+
+	
+	Args:
+		tx_fifo: lista de entrada al conversor. <type 'list'>
+		lanes: una matriz donde cada fila se corresponde a una lane de transmision
+
+	Returns:
+		pc_ready: booleano que indica si tx_fifo tenia suficientes elementos
+		para realizar la conversion de paralelismo.
+
+		tx_fifo: luego de haber extraido elementos para insertar en la matriz de lanes, o
+		una copia del parametro tx_fifo en caso de no tener suficientes elementos para
+		realizar la conversion de paralelismo.
+
+		lanes: luego de haber insertado los elementos extraidos de tx_fifo,inserta
+		un elemento en cada fila de la matriz , o una copia del parametro lanes 
+		en caso de no tener suficientes elementos para realizar la conversion de paralelismo.
+	 
+	"""
+	if ( len(tx_fifo) < len(lanes) ) and  ( (len(tx_fifo) % len(lanes) ) != 0 ) :
+		pc_ready = False
+		return tx_fifo, lanes, pc_ready
+	else :
+		pc_ready = True
+		for i in range(0,len(lanes)) :
+			block = tx_fifo.pop(0)
+			lanes[i].append(block)
+		return tx_fifo , lanes, pc_ready			
+				 
+
+def swap_lanes(nlanes):
+	"""Intercambia elementos de una lista.
+
+	Genera una lista de enteros de 0 a 19 ordenados e intercambia
+	elementos de forma aleatoria, la lista resultante al igual que 
+	la primera no contiene elementos repetidos.
+
+	
+	Args:
+		nlanes: cantidad de elementos para intercambiar, debe ser multiplo de 2 y menor a 20
+
+	Returns:
+		retorna una lista de enteros.
+		ejemplo: nlanes = 2 -> [15, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0, 16, 17, 18, 19]
+	 
+	"""
+	if (nlanes % 2) or nlanes > 20 :
+		raise ValueError('numero de lanes a intercambiar invalido \n\n')
+	
+	else :
+		NLANES = 20
+		swap_index = random.sample(xrange(0, 20), nlanes)
+		order_index = range(0,NLANES)
+		for i in range(0, (nlanes/2) ):
+			lane1 = swap_index.pop(0)
+			lane2 = swap_index.pop(0)
+			order_index[lane1] = lane2
+			order_index[lane2] = lane1
+
+		return	order_index
 
 
 
 
 def list_to_hex(hex_list):
+	"""Genera un numero a partir de una lista.
+	
+	Args:
+		hex_list: lista de enteros de 8 bits
+	Comentarios:
+		Concatena los elementos de una lista de octetos para formar un unico numero equivalente,
+		el numero resutante tiene al octeto de la posicion 0 de la lista
+		como sus bits mas significativos
+	"""
 	result = 0x000000000
-	counter = 8
+	counter = ( len(hex_list)-1 )
 	for value in hex_list:
 		result |= (value << (counter*8))
 		counter-=1
 	#return hex(result).rstrip('L')
 	return result
 
-################# VARIABLES ####################
-NIDLE = 5
-NDATA = 16
+def block_to_hex(block):
+	"""Genera un numero a partir de los valores en un diccionario.
+
+	Concatena los elementos de un bloque para formar un unico numero equivalente,
+	con la forma {sh, block_type, payload}.
+	"""
+	hex_list = []
+	hex_list.append(block['sh'])
+	hex_list.append(block['block_type'])
+	for payload in block['payload'] :
+		hex_list.append(payload)
+	hexnum = list_to_hex(hex_list)
+	return hexnum	
+
 
 ################# ESTADOS ######################
 
@@ -29,20 +121,20 @@ TX_E = 4
 
 state_name = ['TX_INIT', 'TX_C', 'TX_D','TX_T', 'TX_E']
 
-###############################################
+#######################################################
 
 
-############## CGMII CARACTERS########################
+############## CGMII CHARACTERS########################
  
 D0 = 0x70 # 'p'
 D1 = 0x68 # 'h'
 D2 = 0x79 # 'y'
 D3 = 0x73 # 's'
-D4 = 0x69 # 'y'
+D4 = 0x69 # 'i'
 D5 = 0x63 # 'c'
 D6 = 0x61 # 'a'
 D7 = 0x6c # 'l'
- 
+
 S = 0xFB
 T = 0xFD
 I = 0x07
@@ -50,9 +142,14 @@ Q = 0x9C
 Fsig = 0x5C
 Z = 0x00
 E = 0xFE
+############## 100GBE CHARACTERS ######################
+
+I_100G = 0x00
+E_100G = 0x1E
+
 ##############################################
 
-########### RAW_BLOCKS ##############################################
+############ RAW_BLOCKS ################################
 
 DATA_BLOCK = [0x00, D0, D1, D2, D3, D4, D5, D6, D7] 
 
@@ -88,9 +185,6 @@ T7_BLOCK = [0xFF, D0, D1, D2, D3, D4, D5, D6, T]
 # cuidado!! esta codificacion es lo mas parecida posible a la que debemos implementar pero
 # no es exactamente igual
 
-I = 0x00
-E = 0x1E
-
 CODED_DATA_BLOCK = [0x01, D0, D1, D2, D3, D4, D5, D6, D7] 
 
 CODED_START_BLOCK = [0x02,0x78, D1, D2, D3, D4, D5, D6, D7]
@@ -99,7 +193,7 @@ CODED_Q_ORD_BLOCK = [0x02,0x4B, D1, D2, D3, Z, Z, Z, Z]
  
 CODED_Fsig_ORD_BLOCK = [0x02,0x4B, D1, D2, D3, 0XF0, Z, Z, Z]
  
-CODED_IDLE_BLOCK = [0x02,0x1E, I, I, I, I, I, I, I]
+CODED_IDLE_BLOCK = [I, I, I, I, I, I, I] ##cuidado
 
 CODED_ERROR_BLOCK = [0x02,0x1E, E, E, E, E, E, E, E]
  
@@ -123,29 +217,103 @@ CODED_T7_BLOCK   = [0x02,0xFF, D0, D1, D2, D3, D4, D5, D6]
 #desordenar import random
 # random.sample(xrange(0, 20), 20)
 
-##################### ENCODER TABLE ###########################
+##################### ENCODER TABLE ##########################
 
-encoder = { 
-			list_to_hex(DATA_BLOCK)       : list_to_hex(CODED_DATA_BLOCK) ,	
-		    list_to_hex(START_BLOCK)      : list_to_hex(CODED_START_BLOCK) ,
-		    list_to_hex(Q_ORD_BLOCK )     : list_to_hex(CODED_Q_ORD_BLOCK) ,
-		    list_to_hex(Fsig_ORD_BLOCK)   : list_to_hex(CODED_Fsig_ORD_BLOCK) ,
-		    list_to_hex(IDLE_BLOCK)       : list_to_hex(CODED_IDLE_BLOCK) ,
-		    list_to_hex(T0_BLOCK)         : list_to_hex(CODED_T0_BLOCK) ,
-		    list_to_hex(T1_BLOCK)         : list_to_hex(CODED_T1_BLOCK) ,
-		    list_to_hex(T2_BLOCK)         : list_to_hex(CODED_T2_BLOCK) ,
-		    list_to_hex(T3_BLOCK)         : list_to_hex(CODED_T3_BLOCK) ,
-		    list_to_hex(T4_BLOCK)         : list_to_hex(CODED_T4_BLOCK) ,
-		    list_to_hex(T5_BLOCK)         : list_to_hex(CODED_T5_BLOCK) ,
-		    list_to_hex(T6_BLOCK)         : list_to_hex(CODED_T6_BLOCK) ,
-		    list_to_hex(T7_BLOCK)         : list_to_hex(CODED_T7_BLOCK) ,	
-	    		    		    		    		    		    		    		    		    				
-	    	}
+encoder = {
+
+			'DATA_BLOCK' : {'block_name'   : 'CODED_DATA_BLOCK',
+							'sh' 		   : 0x1  ,
+							'block_type'   : D0   ,
+							'payload' 	   : [D1, D2, D3, D4, D5, D6, D7] 
+							} ,
+			
+			'START_BLOCK' : {'block_name'  : 'CODED_START_BLOCK',
+							 'sh' 		   : 0x2  ,
+							 'block_type'  : 0x78 ,
+							 'payload' 	   : [D1, D2, D3, D4, D5, D6, D7]
+							} ,
+
+			'Q_ORD_BLOCK' : {'block_name'  : 'CODED_Q_ORD_BLOCK',
+							 'sh' 		   : 0x2  ,
+							 'block_type'  : 0x4B ,
+							 'payload' 	   : [D1, D2, D3, Z, Z, Z, Z]
+							} ,
+
+		 'Fsig_ORD_BLOCK' : {'block_name'  : 'CODED_Fsig_ORD_BLOCK',
+	   						 'sh' 		   : 0x2  ,
+							 'block_type'  : 0x4B ,
+							 'payload' 	   : [D1, D2, D3, 0XF0, Z, Z, Z]
+							} ,
+
+			'IDLE_BLOCK' : {'block_name'   : 'CODED_IDLE_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0x1E ,
+							'payload' 	   : [I_100G,I_100G,I_100G,I_100G,I_100G,I_100G,I_100G]
+							} ,
+
+			'ERROR_BLOCK' : {'block_name'  : 'CODED_ERROR_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0x1E ,
+							'payload' 	   : [E_100G,E_100G,E_100G,E_100G,E_100G,E_100G,E_100G]
+							} ,
+
+			'T0_BLOCK' 	  : {'block_name'  : 'CODED_T0_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0x87 ,
+							'payload' 	   : [I_100G,I_100G,I_100G,I_100G,I_100G,I_100G,I_100G]
+							} ,
+
+			'T1_BLOCK' 	  : {'block_name'  : 'CODED_T1_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0x87 ,
+							'payload' 	   : [D0,I_100G,I_100G,I_100G,I_100G,I_100G,I_100G]
+							} ,
+
+			'T2_BLOCK' 	  : {'block_name'  : 'CODED_T2_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0xAA ,
+							'payload' 	   : [D0,D1,I_100G,I_100G,I_100G,I_100G,I_100G]
+							} ,
+
+			'T3_BLOCK' 	  : {'block_name'  : 'CODED_T3_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0xB4 ,
+							'payload' 	   : [D0,D1,D2,I_100G,I_100G,I_100G,I_100G]
+							} ,
+
+			'T4_BLOCK' 	  : {'block_name'  : 'CODED_T4_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0xCC ,
+							'payload' 	   : [D0,D1,D2,D3,I_100G,I_100G,I_100G]
+							} ,
+
+			'T5_BLOCK' 	  : {'block_name'  : 'CODED_T5_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0xD2 ,
+							'payload' 	   : [D0, D1, D2, D3, D4, I_100G, I_100G]
+							} ,
+
+			'T6_BLOCK' 	  : {'block_name'  : 'CODED_T6_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0xE1 ,
+							'payload' 	   : [D0, D1, D2, D3, D4, D5, I_100G]
+							} ,
+							
+			'T7_BLOCK' 	  : {'block_name'  : 'CODED_T7_BLOCK',
+							'sh' 		   : 0x2  ,
+							'block_type'   : 0xFF ,
+							'payload' 	   : [D0, D1, D2, D3, D4, D5, D6]
+							} 
+		 }
 
 ################################################################
 
 ####################### ALIGNER MARKERS ########################
 
+"""Los alineadores no estan definidos como listas o diccionarios
+   debido a que se insertan luego de la fase de codificacion y scrambling
+   en la cual los bloques se representan como un unico numero entero.   
+"""
 
 align_marker_list = [ 0x2C16821003E97DEff ,
 					  0x29D718E00628E71ff ,
@@ -173,10 +341,30 @@ align_marker_list = [ 0x2C16821003E97DEff ,
 
 
 
-def reverse_byte(num):  
+def reverse_byte(num):
+	"""Invierte el orden de los bits de un numero
+	
+	Ejemplo:
+		param: num = 225 = 0xE1 = 0b11100001 ; return: 135 = 0x87 = 0b10000111
+	Comentario:
+		esta funcion en necesaria por la manera en que el estandar define la  transmision de datos		
+	"""
 	binary = bin(num) 
 	reverse = binary[-1:1:-1] 
 	return int(reverse,2)	
+
+def align_marker_insertion(lanes,block_counter):
+	"""Inserta los marcadores correspondientes a cada lane solo si
+   	   ya fueron enviados 16383 bloques por cada lane (AM_BLOCK_GAP - 1).	
+	"""
+	AM_BLOCK_GAP = 16384
+	if ( (block_counter % AM_BLOCK_GAP) == 0 ):
+		for i in range(0, len(lanes)):
+			AM = align_marker_list[i]
+			lanes[i].insert(0,AM)
+		return lanes
+	else :
+		return lanes		
 
 
 
@@ -234,18 +422,23 @@ class CgmiiFSM(object) :
 		self.data_counter = 0
 		self.idle_counter = 0
 		self.state_sequence =[]
-		#self.variables = [0,0,0] #aca voy a almacenar en el siguiente orden: [estado actual,prox estado,bloque enviado]
+		
 	def print_state_seq(self):
 		for i in self.state_sequence:
 			print '\n\n',i
 
-	def change_state(self,force_error): # note that the first argument is self
-		self.variables = [0,0,0]
+	def change_state(self,force_error = False):
+		# en self.variables voy a almacenar en el siguiente orden: [estado actual,prox estado,bloque enviado]
+		self.variables = []
+
 		if force_error :
 			print 'ERROR FORZADO \n'
+			'''
+			dado el estado actual generar un bloque invalido
+			'''
 		else :
 			if self.state == TX_INIT :
-				self.variables[0] = state_name[self.state] # LOGEO
+				self.variables.append( state_name[self.state] ) # LOGEO
 				
 				#######funcionalidad#####
 				self.tx_raw = IDLE_BLOCK
@@ -253,12 +446,12 @@ class CgmiiFSM(object) :
 				self.state = TX_C
 				#########################
 
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
-				self.state_sequence.append(self.variables) # LOGEO
+				self.variables.append( state_name[self.state]) # LOGEO
+				self.variables.append( self.tx_raw )		   # LOGEO
+				self.state_sequence.append(self.variables)     # LOGEO
 
 			elif self.state == TX_C:
-				self.variables[0] = state_name[self.state] # LOGEO
+				self.variables.append( state_name[self.state] ) # LOGEO
 
 				########funcionalidad#########
 				
@@ -272,13 +465,13 @@ class CgmiiFSM(object) :
 					self.data_counter+=1
 				#############################
 				
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
+				self.variables.append( state_name[self.state] ) # LOGEO
+				self.variables.append( self.tx_raw	)		   # LOGEO
 				self.state_sequence.append(self.variables) # LOGEO
 				
 	
 			elif self.state == TX_D:
-				self.variables[0] = state_name[self.state] # LOGEO
+				self.variables.append( state_name[self.state] ) # LOGEO
 
 				######funcionalidad###########
 				if self.data_counter < NDATA:
@@ -290,13 +483,13 @@ class CgmiiFSM(object) :
 					self.state = TX_T
 				#############################
 
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
+				self.variables.append( state_name[self.state] )# LOGEO
+				self.variables.append( self.tx_raw  )		   # LOGEO
 				self.state_sequence.append(self.variables) # LOGEO
 				
 
 			elif self.state == TX_T:
-				self.variables[0] = state_name[self.state] # LOGEO
+				self.variables.append( state_name[self.state] ) # LOGEO
 
 				#####funcionalidad############
 				self.tx_raw = IDLE_BLOCK
@@ -305,23 +498,23 @@ class CgmiiFSM(object) :
 				self.data_counter = 0
 				##############################
 
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
+				self.variables.append( state_name[self.state] )# LOGEO
+				self.variables.append( self.tx_raw	)		   # LOGEO
 				self.state_sequence.append(self.variables) # LOGEO
 
 				
 			elif self.state == TX_E:
-				self.variables[0] = state_name[self.state] # LOGEO
+				self.variables.append( state_name[self.state] )# LOGEO
 
 				####funcionalidad#######
 				self.tx_raw = ERR_BLOCK
 				self.state = TX_E
 				########################			         
 
-				self.variables[1] = state_name[self.state] # LOGEO
-				self.variables[2] = self.tx_raw			   # LOGEO
+				self.variables.append( state_name[self.state] )# LOGEO
+				self.variables.append( self.tx_raw	)		   # LOGEO
 				self.state_sequence.append(self.variables) # LOGEO		
 
 
 #############################################################################################################
-#############################################################################################################				
+#############################################################################################################
