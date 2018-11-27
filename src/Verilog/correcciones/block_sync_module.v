874629be58711f8738ed3491b31edb6dabdd1d4c
Diego Abratte <diego.abratte@alumnos.unc.edu.ar>
2018-11-27 16:01:17 -0300

    am lock completo,sin testear

--- src/Verilog/correcciones/block_sync_module.v
+++ src/Verilog/correcciones/block_sync_module.v
@@ -2,11 +2,13 @@
 
 module block_sync_module
 #( 
-    parameter LEN_CODED_BLOCK     = 66,
+    parameter LEN_CODED_BLOCK    = 66,
     parameter MAX_INVALID_SH     = 6,
     parameter MAX_WINDOW         = 2048,
+    parameter UNLOCKED_WINDOW    = 64,
+    parameter LOCKED_WINDOW      = 1024;
     parameter NB_WINDOW_CNT      = $clog2(MAX_WINDOW),
-    parameter NB_INVALID_CNT     = $clog2(MAX_INVALID_AM)
+    parameter NB_INVALID_CNT     = $clog2(MAX_INVALID_SH)
 
  )
  (
@@ -16,7 +18,9 @@ module block_sync_module
     input wire                        i_enable,
     input wire 						  i_valid, //valid signal from serial_to_parallel converter(means 66bit acumulation ready)
     input wire                        i_signal_ok,
-    //input wire i_control                                          // por ahora no se usa,indicar escritura de param de control
+    //input wire i_control                                          // por ahora no se usa,
+                                                                    //indica escritura de param de control
+
     //input  wire [NB_WINDOW_CNT -1 : 0] i_unlocked_count_limit   , //controla parametros de fsm
     //input  wire [NB_WINDOW_CNT -1 : 0] i_locked_count_limit     , //controla parametros de fsm
     //input  wire [NB_INVALID_CNT-1 : 0] i_sh_invalid_limit       , //controla parametros de fsm
@@ -31,15 +35,23 @@ localparam LEN_EXTENDED_BLOCK = LEN_CODED_BLOCK*2;
 localparam LEN_INDEX = $clog2(LEN_CODED_BLOCK);
 
 //INTERNAL SIGNALS
+
+//FSM control
+reg [NB_WINDOW_CNT -1 : 0] unlocked_count_limit;
+reg [NB_WINDOW_CNT -1 : 0] locked_count_limit;
+reg [NB_INVALID_CNT-1 : 0] sh_invalid_limit;
+
 reg  [LEN_CODED_BLOCK-1 : 0]    data_prev;
-wire [LEN_INDEX-1 : 0] index;
 wire [LEN_EXTENDED_BLOCK-1 : 0] data_ext;
 wire [LEN_CODED_BLOCK-1 : 0]    data_shifted;
+wire [LEN_INDEX-1 : 0]          index;
 wire                            sh_valid;
-assign data_ext = {data_prev,i_data};
+
+
+assign data_ext     = {data_prev,i_data};
 assign data_shifted = data_ext[(LEN_EXTENDED_BLOCK-1-index) -:LEN_CODED_BLOCK] ;
-assign o_data = data_shifted;
-assign sh_valid = ^(data_shifted[LEN_CODED_BLOCK-1 -: 2]);
+assign o_data       = data_shifted;
+assign sh_valid     = ^(data_shifted[LEN_CODED_BLOCK-1 -: 2]);
 
 //Update data
 always @ (posedge i_clock)
@@ -53,11 +65,16 @@ always @ (posedge i_clock)
 begin
     if(i_reset)
     begin
-
+        sh_invalid_limit     <= MAX_INVALID_SH;
+        locked_count_limit   <= LOCKED_WINDOW;
+        unlocked_count_limit <= UNLOCKED_WINDOW
     end
     /***  POR AHORA LOS PARAMETROS DE CONTROL SON ESTATICOS
     else if(i_enable && i_control)
     begin
+        sh_invalid_limit     <= i_sh_invalid_limit    ;
+        locked_count_limit   <= i_locked_count_limit  ;
+        unlocked_count_limit <= i_unlocked_count_limit;
     end
     */
 end
@@ -68,19 +85,24 @@ end
 block_sync_fsm
 #(
     .LEN_CODED_BLOCK(LEN_CODED_BLOCK),
-    .MAX_INVALID_AM(MAX_INVALID_AM),
+    .MAX_INVALID_SH(MAX_INVALID_SH),
     .MAX_WINDOW(MAX_WINDOW),
     .NB_WINDOW_CNT(NB_WINDOW_CNT),
     .NB_INVALID_CNT(NB_INVALID_CNT)
  )
     u_block_sync_fsm
     (
-        .i_clock    (i_clock),
-        .i_reset    (i_reset),
-        .i_enable   (i_enable),
-        .i_signal_ok(i_signal_ok),
-        .i_sh_valid (sh_valid),
-        .o_index    (index)
+        .i_clock                (i_clock),
+        .i_reset                (i_reset),
+        .i_enable               (i_enable),
+        .i_valid                (i_valid),
+        .i_signal_ok            (i_signal_ok),
+        .i_sh_valid             (sh_valid),
+        .i_unlocked_count_limit (unlocked_count_limit),
+        .i_locked_count_limit   (locked_count_limit),
+        .i_sh_invalid_limit     (sh_invalid_limit)
+
+        .o_index                (index)
     );
 
 
---
+++ src/Verilog/verilog_tests/am_error_counter.v
@@ -0,0 +1,66 @@
+
+
+
+module am_error_counter
+#(
+	parameter NB_BIP = 8,
+	parameter NB_COUNTER = 32
+ )
+ (
+ 	input wire 						i_clock,
+ 	input wire 						i_reset,
+ 	input wire 						i_enable,
+ 	input wire 						i_match,
+ 	input wire [NB_BIP-1 : 0] 		i_recived_bip,
+ 	input wire [NB_BIP-1 : 0] 		i_calculated_bip,
+
+ 	output wire [NB_COUNTER-1 : 0] 	o_error_count,
+ 	output wire 				  	o_overflow_flag
+
+ );
+//LOCALPARAM
+
+localparam NB_CURRENT_ERROR = $clog2(NB_BIP);
+
+
+//INTERNAL SIGANLS
+
+reg [NB_CURRENT_ERROR-1 : 0] error_counter,error_counter_next;
+reg 						 overflow_flag;
+
+
+//Update counter
+ always @ (posedge i_clock)
+ begin
+
+ 	if(i_reset) // se deberia agregar una condicion p resetear contador i_reset_count
+ 	begin
+ 		error_counter <= {NB_COUNTER{1'b0}};
+ 		overflow_flag <= 1'b0;
+ 	end
+
+ 	else if (i_enable && i_match)
+
+ 		if(error_counter[NB_COUNTER-1 : NB_CURRENT_ERROR] == {NB_COUNTER-NB_CURRENT_ERROR{1'b1}})//** check
+ 		begin//evitar overflow
+ 			error_counter <= {NB_COUNTER{1'b1}};
+ 			overflow_flag <= 1'b1;
+ 		end
+ 		else
+ 		begin
+ 			error_counter <= error_counter + error_counter_next;
+ 			overflow_flag <= 1'b0;
+ 		end
+ end
+
+
+ always @ *
+ begin
+ 	error_counter_next = 0;
+ 	for(integer i=0; i<NB_BIP; i=i+1)
+ 	begin
+ 		if(i_recibed_bip[i] != i_calculated_bip)
+ 			error_counter_next  = error_counter_next + 1'b1;
+ 	end
+
+ end
