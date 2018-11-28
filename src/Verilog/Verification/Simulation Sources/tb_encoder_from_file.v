a3dbda12fae24cf583f7cb89f3c704dd8f8ae77f
Diego Abratte <diego.abratte@alumnos.unc.edu.ar>
2018-11-28 10:18:11 -0300

    second loopback with vector matching

--- src/Verilog/Verification/Simulation Sources/tb_encoder_from_file.v
+++ src/Verilog/Verification/Simulation Sources/tb_encoder_from_file.v
@@ -2,20 +2,21 @@
 
 module tb_encoder_comparator   ;
 
-parameter LEN_TX_CTRL 		    = 8;
-parameter LEN_TX_DATA 		    = 64;
-parameter LEN_TX_TYPE		    = 4;
+parameter LEN_TX_CTRL           = 8;
+parameter LEN_TX_DATA           = 64;
+parameter LEN_TX_TYPE           = 4;
 parameter LEN_CODED_BLOCK       = 66;
 parameter LEN_RX_DATA           = 64;
 parameter LEN_RX_CTRL           = 8;
 parameter LEN_TYPE              = 4;
+parameter SEED                      = 58'd0;
 
 
 //REGISTROS GENERALES
 reg                             tb_clock;
 reg                             tb_reset;
 reg                             tb_enable;
-
+reg                             tb_bypass;
 //REGISTROS PARA ENCODER FROM FILE
 reg  [LEN_TX_CTRL-1:0]          tb_tx_ctrl;
 reg  [LEN_TX_DATA-1:0]          tb_tx_data;
@@ -26,9 +27,11 @@ reg  [0 : LEN_CODED_BLOCK-1]    temp_tx_coded;
 //REGISTROS PARA ENCODER_FSM
 wire [LEN_CODED_BLOCK-1:0]      tb_tx_coded;
 wire [LEN_CODED_BLOCK-1:0]      tb_fsm_tx_coded;
-wire [LEN_TYPE-1 : 0] 	        tb_o_type;
+wire [LEN_TYPE-1 : 0]           tb_o_type;
 
 //REGISTROS PARA DECODER
+wire [LEN_CODED_BLOCK-1 : 0]    tb_rx_coded;
+wire [LEN_CODED_BLOCK-1 : 0]    tb_rx_coded_next;
 wire [LEN_RX_DATA-1 : 0]        tb_rx_data;
 wire [LEN_RX_CTRL-1 : 0]        tb_rx_ctrl;
 wire [LEN_TYPE-1:0]             tb_rx_type;
@@ -40,6 +43,12 @@ wire [LEN_TYPE-1:0]             tb_rtype_next_out;
 //REGISTROS DE SALIDA DEL DECODER (TO CGMII)
 wire [LEN_RX_DATA-1 : 0]        tb_rx_raw_data;
 wire [LEN_RX_CTRL-1 : 0]        tb_rx_raw_ctrl;
+
+
+//scrambler
+wire [LEN_CODED_BLOCK-1 : 0]    tb_scrambled_data;
+wire [LEN_CODED_BLOCK-1 : 0]    tb_descrambled_data;
+
 
 integer                         fid_tx_data;
 integer                         fid_tx_ctrl;
@@ -53,32 +62,33 @@ integer                         ptr_coded;
 
 initial
 begin
-	
-	fid_tx_ctrl= $fopen("/media/ramiro/1C3A84E93A84C16E/Fundacion/PPS/src/Python/file_generator/encoder-input-ctrl.txt", "r");
-		if(fid_tx_ctrl==0)
-		begin
-			$display("\n\nLa entrada para Tx-Ctrl no pudo ser abierta\n\n");
-			$stop;
-		end
-	fid_tx_data= $fopen("/media/ramiro/1C3A84E93A84C16E/Fundacion/PPS/src/Python/file_generator/encoder-input-data.txt", "r");
-		if(fid_tx_data==0)
-		begin
-			$display("\n\nLa entrada para Tx-Data no pudo ser abierta\n\n");
-			$stop;
-		end
-		
-	fid_tx_coded= $fopen("/media/ramiro/1C3A84E93A84C16E/Fundacion/PPS/src/Python/file_generator/encoder-output.txt", "r");
+    
+    fid_tx_ctrl= $fopen("/home/diego/fundacion/PPS/src/Python/file_generator/encoder-input-ctrl.txt", "r");
+        if(fid_tx_ctrl==0)
+        begin
+            $display("\n\nLa entrada para Tx-Ctrl no pudo ser abierta\n\n");
+            $stop;
+        end
+    fid_tx_data= $fopen("/home/diego/fundacion/PPS/src/Python/file_generator/encoder-input-data.txt", "r");
+        if(fid_tx_data==0)
+        begin
+            $display("\n\nLa entrada para Tx-Data no pudo ser abierta\n\n");
+            $stop;
+        end
+        
+    fid_tx_coded= $fopen("/home/diego/fundacion/PPS/src/Python/file_generator/encoder-output.txt", "r");
         if(fid_tx_coded==0)
         begin
            $display("\n\nLa entrada para Tx-Coded no pudo ser abierta\n\n");
            $stop;
         end
 
-	tb_reset  = 1'b1 ;
-	tb_clock  = 1'b0 ;
-	tb_enable = 1'b0 ;
-	tb_enable = 1'b1 ;
-#6	tb_reset  = 1'b0 ;
+    tb_reset  = 1'b1 ;
+    tb_clock  = 1'b0 ;
+    tb_enable = 1'b0 ;
+    tb_bypass = 1'b0 ;
+#6  tb_reset  = 1'b0 ;
+    tb_enable = 1'b1 ;
 end
 
 
@@ -130,21 +140,21 @@ end
 
 
 encoder_comparator
-	#(
-	.LEN_TX_CTRL    (LEN_TX_CTRL)     ,
-	.LEN_CODED_BLOCK(LEN_CODED_BLOCK) ,
-	.LEN_TX_DATA    (LEN_TX_DATA)
- 	)
+    #(
+    .LEN_TX_CTRL    (LEN_TX_CTRL)     ,
+    .LEN_CODED_BLOCK(LEN_CODED_BLOCK) ,
+    .LEN_TX_DATA    (LEN_TX_DATA)
+    )
 u_encoder_comparator
-	(
-	.i_clock   (tb_clock)    ,
-	.i_reset   (tb_reset)    ,
-	.i_enable  (tb_enable)   ,
-	.i_tx_ctrl (tb_tx_ctrl)  ,
-	.i_tx_data (tb_tx_data)  ,
-	.o_tx_type  (tb_o_type)  ,
-	.o_tx_coded(tb_tx_coded)
-	);
+    (
+    .i_clock   (tb_clock)    ,
+    .i_reset   (tb_reset)    ,
+    .i_enable  (tb_enable)   ,
+    .i_tx_ctrl (tb_tx_ctrl)  ,
+    .i_tx_data (tb_tx_data)  ,
+    .o_tx_type  (tb_o_type)  ,
+    .o_tx_coded(tb_tx_coded)
+    );
 
 encoder_fsm
     #(
@@ -159,10 +169,36 @@ u_encoder_fsm
     .i_tx_coded(tb_tx_coded) ,
     .o_tx_coded(tb_fsm_tx_coded)
     );
+scrambler
+    #(
+        .SEED(SEED)
+     )
+        u_scrambler
+        (
+            .i_clock   (tb_clock)    ,
+            .i_reset   (tb_reset)    ,
+            .i_enable  (tb_enable)   ,
+            .i_bypass   (tb_bypass)  ,
+            .i_data    (tb_fsm_tx_coded),
+            .o_data     (tb_scrambled_data)
+        );
+descrambler
+    #(
+        .SEED(SEED) 
+     )
+    u_descrambler
+    (
+            .i_clock   (tb_clock)    ,
+            .i_reset   (tb_reset)    ,
+            .i_enable  (tb_enable)   ,
+            .i_bypass   (tb_bypass)  ,
+            .i_data    (tb_scrambled_data),
+            .o_data     (tb_descrambled_data)
+    );
 
 decoder_comparator
     #(
-	.LEN_RX_CTRL    (LEN_RX_CTRL)     ,
+    .LEN_RX_CTRL    (LEN_RX_CTRL)     ,
     .LEN_CODED_BLOCK(LEN_CODED_BLOCK) ,
     .LEN_RX_DATA    (LEN_RX_DATA)
     )
@@ -171,7 +207,7 @@ u_decoder_comparator
     .i_clock    (tb_clock)          ,
     .i_reset    (tb_reset)          ,
     .i_enable   (tb_enable)         ,
-    .i_rx_coded (tb_fsm_tx_coded)   ,
+    .i_rx_coded (tb_descrambled_data)   ,
     .o_rx_data  (tb_rx_data)        ,
     .o_rx_ctrl  (tb_rx_ctrl)        ,
     .o_rx_type  (tb_rx_type)
@@ -182,7 +218,8 @@ decoder_fsm_interface
     #(
     .LEN_TYPE(LEN_TYPE)
     )
-u_decoder_fsm_interface(
+    u_decoder_interface
+    (
     .i_clock        (tb_clock)          ,
     .i_reset        (tb_reset)          ,
     .i_enable       (tb_enable)         ,
@@ -196,16 +233,17 @@ decoder_fsm
     .LEN_RX_DATA(LEN_RX_DATA),
     .LEN_RX_CTRL(LEN_RX_CTRL)
     )
-u_decoder_fsm(
-    .i_clock            (tb_clock)          ,
-    .i_reset            (tb_reset)          ,
-    .i_enable           (tb_enable)         ,
-    .i_r_type           (tb_rtype_out)      ,
-    .i_r_type_next      (tb_rtype_next_out) ,
-    .i_rx_data          (tb_rx_data)        ,
-    .i_rx_control       (tb_rx_ctrl)        ,
-    .o_rx_raw_data      (tb_rx_raw_data)    ,
-    .o_rx_raw_control   (tb_rx_raw_ctrl)
+    u_decoder_fsm
+    (
+    .i_clock        (tb_clock)          ,
+    .i_reset        (tb_reset)          ,
+    .i_enable       (tb_enable)         ,
+    .i_r_type       (tb_rtype_out)      ,
+    .i_r_type_next  (tb_rtype_next_out) ,
+    .i_rx_data      (tb_rx_data)        ,
+    .i_rx_control      (tb_rx_ctrl)        ,
+    .o_rx_raw_data  (tb_rx_raw_data)    ,
+    .o_rx_raw_control  (tb_rx_raw_ctrl)
     );
    
 endmodule
