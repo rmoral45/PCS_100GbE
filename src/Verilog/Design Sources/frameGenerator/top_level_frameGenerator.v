7c8039fba05f0de5811802afc1492d15df96a73c
Ramiro Moral <rmoral45@gmail.com>
2019-03-12 09:08:26 -0300

    Frame Generator con tb listo

--- src/Verilog/Design Sources/frameGenerator/top_level_frameGenerator.v
+++ src/Verilog/Design Sources/frameGenerator/top_level_frameGenerator.v
@@ -1,39 +1,40 @@
 module top_level_frameGenerator
 	#(
-	parameter						LEN_TX_DATA		= 64,
-	parameter						LEN_TX_CTRL		= 8
+	parameter							LEN_DATA_BLOCK		= 64,
+	parameter							LEN_CTRL_BLOCK		= 8
 	)
 	(
-	input							i_clock,
-	input							i_reset,
-	output wire	[LEN_TX_DATA-1 : 0]	o_tx_data,
-	output wire	[LEN_TX_CTRL-1 : 0]	o_tx_ctrl
+	input								i_clock,
+	input								i_reset,
+	input								i_enable,
+	output wire	[LEN_DATA_BLOCK-1 : 0]	o_tx_data,
+	output wire	[LEN_CTRL_BLOCK-1 : 0]	o_tx_ctrl
 	);
 
 
 //Registros y parametros para parametrizacion y generacion de ruido
-localparam							LEN_GNG			= 16;
-localparam							NB_TERM			= 3;
-localparam							NB_DATA			= 8;
-localparam							NB_IDLE			= 5;
-wire								noise_enable;
-wire                                noise_valid;
-wire [LEN_GNG-1:0]					noise_data;
-wire [NB_TERM-1:0]					nterm;
-wire [NB_DATA-1:0]					ndata;
-wire [NB_IDLE-1:0]					nidle;
+localparam								LEN_GNG			= 16;
+localparam								NB_TERM			= 3;
+localparam								NB_DATA			= 8;
+localparam								NB_IDLE			= 5;
+wire									noise_enable;
+wire                                	noise_valid;
+wire [LEN_GNG-1:0]						noise_data;
+wire [NB_TERM-1:0]						nterm;
+wire [NB_DATA-1:0]						ndata;
+wire [NB_IDLE-1:0]						nidle;
 
-assign 								noise_enable 	= 1'b1;
-assign  							nterm 			= noise_data[LEN_GNG-8 -: NB_TERM];	//3 bits mas significativos para establecer el numero de terminate
-assign  							ndata 			= (noise_data[LEN_GNG-4 -: NB_DATA] > 8'b1100100) ? 
+assign 									noise_enable 	= 1'b1;
+assign  								nterm 			= noise_data[LEN_GNG-8 -: NB_TERM];	//3 bits mas significativos para establecer el numero de terminate
+assign  								ndata 			= (noise_data[LEN_GNG-4 -: NB_DATA] > 8'b1100100) ? 
                                                         8'b11000110 : noise_data[LEN_GNG-4 -: NB_DATA]; //8 bits siguientes para data
-assign  							nidle 			= noise_data[LEN_GNG-8 -: NB_IDLE]; //6 bits menos significativos para idle
+assign  								nidle 			= noise_data[LEN_GNG-8 -: NB_IDLE]; //6 bits menos significativos para idle
 
 
 
 //Registros y parametros para el generador de frames
-wire								frameGenerator_enb;
-assign 								frameGenerator_enb	= (noise_valid == 1'b1) ? 1'b1 : 1'b0;
+wire									frameGenerator_enb;
+assign 									frameGenerator_enb	= (noise_valid == 1'b1) ? 1'b1 : 1'b0;
 
 frameGenerator#(
 	)
---
+++ src/Verilog/Design Sources/toplevel_primerLoopback.v
@@ -0,0 +1,63 @@
+module toplevel_primerLoopback
+	#(
+	parameter							LEN_DATA_BLOCK 	= 64,
+	parameter							LEN_CTRL_BLOCK 	= 8,
+	parameter							LEN_CODED_BLOCK	= 66,
+	parameter							TX_NMODULES		= 2,
+	parameter							RX_NMODULES 	= 2
+	)
+	(
+	input 								i_clock,
+	input 								i_reset,
+	input 								i_enable_frameGenerator,
+	input		[TX_NMODULES-1 : 0]		i_enable_tx,
+	input		[RX_NMODULES-1 : 0]		i_enable_rx,
+	output wire	[LEN_DATA_BLOCK-1 : 0]	o_rx_raw_data,
+	output wire	[LEN_CTRL_BLOCK-1 : 0]	o_rx_raw_ctrl	
+	);
+
+	wire		[LEN_DATA_BLOCK-1 : 0]	encoder_data_input;
+	wire		[LEN_CTRL_BLOCK-1 : 0]	encoder_ctrl_input;
+	wire		[LEN_CODED_BLOCK-1 : 0]	scrambled_data;
+
+top_level_frameGenerator
+	#(
+	.LEN_DATA_BLOCK(LEN_DATA_BLOCK),
+	.LEN_CTRL_BLOCK(LEN_CTRL_BLOCK)
+	)
+u_top_level_frameGenerator
+	(
+	.i_clock(i_clock),
+	.i_reset(i_reset),
+	.i_enable(i_enable_frameGenerator),
+	.o_tx_data(encoder_data_input),
+	.o_tx_ctrl(encoder_ctrl_input)
+	);
+
+tx_modules
+	#(
+	)
+u_tx_modules
+	(
+	.i_clock(i_clock),
+	.i_reset(i_reset),
+	.i_tx_data(encoder_data_input),
+	.i_tx_ctrl(encoder_ctrl_input),
+	.i_enable(i_enable_tx),
+	.o_scrambled_data(scrambled_data)
+	);
+
+rx_modules
+	#(
+	)
+u_rx_modules
+	(
+	.i_clock(i_clock),
+	.i_reset(i_reset),
+	.i_enable(i_enable_rx),
+	.i_scrambled_data(scrambled_data),
+	.o_rx_raw_data(o_rx_raw_data),
+	.o_rx_raw_ctrl(o_rx_raw_ctrl)
+	);
+
+endmodule
