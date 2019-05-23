72834bdc9ac20fd0b387b70396575f96d6b3fa12
rmoral45 <rmoral45@gmail.com>
2019-05-23 10:28:01 -0300

    asd

--- src/Python/modules/deskew_calculator_v2.py
+++ src/Python/modules/deskew_calculator_v2.py
@@ -1,8 +1,13 @@
 
+
 class deskewCalculator(object):
     
     def __init__(self, nlanes):
-        self._nlanes = nlanes
+        self._nlanes        = nlanes
+        self._counters      = for(ss_counter for i in range(nlanes))
+        self._stop_signals  = [0]*nlanes
+        self._start_signal  = 0
+        self._state         = "INIT"
 
     
 
@@ -10,5 +15,31 @@ class deskewCalculator(object):
 class ss_counter(object):
 
     def __init__(self, MAX_SKEW):
-        self.start  = 0
-        self.stop   = 0
+        self._max_skew  = 0
+        self._start     = 0
+        self._stop      = 0
+        self._count     = 0
+        self._finish    = 0
+        self._final_count = 0 
+
+    def reset(self):
+        self._max_skew  = 0
+        self._start     = 0
+        self._stop      = 0
+        self._count     = 0
+        self.finish     = 0
+        self._final_count = 0 
+
+    def update_count(self, start_signal, stop_signal, resync_signal):
+        if(resync_signal):
+            self.reset()
+        elif stop_signal and start_signal: 
+            self._final_count = self._count
+            self._finish = 1
+        elif start_signal and not stop_signal:
+            self._start = 1
+        elif start_signal and not self._finish
+            self._count += 1
+
+
+    
