14e80fdb9cda0140ae42c570c7a94fdd1cb451a6
rmoral45 <rmoral45@gmail.com>
2019-05-23 10:05:04 -0300

    asds

--- src/Python/modules/deskew_calculator_v2.py
+++ src/Python/modules/deskew_calculator_v2.py
@@ -5,4 +5,10 @@ class deskewCalculator(object):
         self._nlanes = nlanes
 
     
-    
+
+
+class ss_counter(object):
+
+    def __init__(self, MAX_SKEW):
+        self.start  = 0
+        self.stop   = 0
