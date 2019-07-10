19a5c55073b3e4940b8fdb733cb14661a0ffde1c
rmoral45 <rmoral45@gmail.com>
2019-07-10 10:52:34 -0300

    integrando runneable de deskew con su modulo

--- src/Python/run/skew_tb_files/run_deskew_calculator.py
+++ src/Python/run/skew_tb_files/run_deskew_calculator.py
@@ -10,17 +10,27 @@ NCLOCK      = 1000
 #AM_PERIOD   = 16384 #chequear si no es 16383
 AM_PERIOD   = 30
 
+
+'''
+Script destinado a generar las seniales de control para el bloque deskew.
+Se tienen 2 matrices, una de start of lanes y otra de resync.
+Cada columna de la matriz representa un instante de tiempo. 
+Cada vez que llega el primer start of lane de alguna linea, simulamos tambien un resync.
+Existe otro vector de am_lock. Tiene tamanio NLANES e indica cuales lineas estan lockeadas.
+Cuando este vector tenga todos sus elementos en 1, se declarara el am_lock de todas las lineas.
+'''
+
 def main():
 
     sol_matrix = [[0 for ncols in range(NLANES)] for nrows in range(NCLOCK)]
     resync_vector = [[0 for ncols in range(NLANES)] for nrows in range(NCLOCK)]
-    am_lock_lanes = [0]*NLANES
     am_lock_vector = [0]*NCLOCK
+    am_lock_lanes = [0]*NLANES
 
     simulate_skew(sol_matrix, resync_vector, am_lock_lanes)
 
     for clock in range(NCLOCK):
-        if(sum(am_lock_vector) == NLANES):
+        if(sum(am_lock_lanes) == NLANES):
             am_lock_vector[clock] = 1
 
 def simulate_skew(sol_matrix, resync_vector, am_lock_lanes):
@@ -28,8 +38,8 @@ def simulate_skew(sol_matrix, resync_vector, am_lock_lanes):
     delay_vector = [0,0,5,3,4,5,6,7,8,9,10,5,12,13,14,15,16,17,18,19]
 
     for index, value in enumerate(delay_vector):
-        sol_matrix[value][index] = 1
-        resync_vector[value][index] = 1
+        sol_matrix[index][value] = 1
+        resync_vector[index][value] = 1
         sol_matrix[value+AM_PERIOD*index][index] = 1
         am_lock_lanes[index] = 1
         bp()
