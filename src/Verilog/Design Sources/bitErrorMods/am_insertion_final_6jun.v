aca5f6f969620c0f6310fb77ac5e731bd652004c
rmoral45 <rmoral45@gmail.com>
2019-10-24 14:35:13 -0300

    Agregamos señal de valid desde PC_1_to_20 en am_insertion

--- src/Verilog/Design Sources/bitErrorMods/am_insertion_final_6jun.v
+++ src/Verilog/Design Sources/bitErrorMods/am_insertion_final_6jun.v
@@ -3,7 +3,7 @@
 
 module am_insertion
 #(
-	parameter 							NB_CODED_BLOCK  = 66,
+	parameter 							LEN_CODED_BLOCK  = 66,
 	parameter 							AM_ENCODING_LOW  = 24'd0, //{M0,M1,M2} tabla 82-2
 	parameter 							AM_ENCODING_HIGH = 24'd0,  //{M4,M5,M6} tabla 82-2
 	parameter 							NB_BIP = 8
@@ -12,10 +12,11 @@ module am_insertion
  	input  wire 						i_clock ,
  	input  wire 						i_reset ,
  	input  wire 						i_enable,
+ 	input  wire                         i_valid,
  	input  wire 						i_am_insert,
- 	input  wire [NB_CODED_BLOCK-1 : 0] 	i_data,
+ 	input  wire [LEN_CODED_BLOCK-1 : 0] i_data,
 	
- 	output wire [NB_CODED_BLOCK-1 : 0]	o_data
+ 	output wire [LEN_CODED_BLOCK-1 : 0]	o_data
 
  );
 
@@ -23,19 +24,30 @@ module am_insertion
 localparam CTRL_SH = 2'b10;
 
 //INTERNAL SIGNALS
-reg  [NB_CODED_BLOCK-1 : 0] data;
+reg  [LEN_CODED_BLOCK-1 : 0] data;
 wire [NB_BIP-1 : 0] bip3, bip7;
 wire static_start_of_lane = 1'b0; 
 
 
+/*always @ *
+begin
+
+	data = {LEN_CODED_BLOCK{1'b0}};
+	if (i_enable && ~i_am_insert)
+		data = i_data;
+
+	else if (i_enable && i_am_insert)
+		data = {CTRL_SH,AM_ENCODING_LOW,bip3,AM_ENCODING_HIGH,bip7};
+end*/
+
 always @ *
 begin
-	data = {NB_CODED_BLOCK{1'b0}};
- 	if (i_enable && ~i_am_insert)
- 		data = i_data;
+    
+    data = i_data;
+    
+    if(i_am_insert)
+        data = {CTRL_SH,AM_ENCODING_LOW,bip3,AM_ENCODING_HIGH,bip7};
 
- 	else if (i_enable && i_am_insert)
- 		data <= {CTRL_SH,AM_ENCODING_LOW,bip3,AM_ENCODING_HIGH,bip7};
 end
 
 //PORTS
@@ -44,7 +56,7 @@ assign o_data = data;
 //instances
 bip_calculator
 #(
-	.LEN_CODED_BLOCK(NB_CODED_BLOCK)
+	.LEN_CODED_BLOCK(LEN_CODED_BLOCK)
  )
 	u_bip_calculator
  	(
@@ -52,6 +64,7 @@ bip_calculator
         .i_reset 		(i_reset)  ,
      	.i_data  		(data)     , 
         .i_enable 		(i_enable) ,
+        .i_valid        (i_valid)  ,
 		.i_am_insert	(i_am_insert),
 		.i_start_of_lane(static_start_of_lane),
         .o_bip3			(bip3),
--- src/Verilog/Design Sources/bitErrorMods/am_insertion_toplevel.v
+++ src/Verilog/Design Sources/bitErrorMods/am_insertion_toplevel.v
@@ -2,8 +2,8 @@
 
 module am_insertion_toplevel
 #(
-	parameter 							            NB_TAGGED_BLOCK     = 67,
-    parameter                                       NB_CODED_BLOCK      = 66,
+	parameter 							            LEN_TAGGED_BLOCK     = 67,
+    parameter                                       LEN_CODED_BLOCK      = 66,
     parameter                                       N_LANES             = 20,
 	parameter 							            AM_ENCODING_LOW     = 24'd0, //{M0,M1,M2} tabla 82-2
 	parameter 							            AM_ENCODING_HIGH    = 24'd0,  //{M4,M5,M6} tabla 82-2
@@ -14,17 +14,17 @@ module am_insertion_toplevel
     input wire                                      i_reset,
     input wire                                      i_valid,
     input wire                                      i_enable,
-    input wire  [(NB_TAGGED_BLOCK*N_LANES)-1 : 0]   i_data,
+    input wire  [(LEN_TAGGED_BLOCK*N_LANES)-1 : 0]  i_data,
 
-    output wire [(NB_CODED_BLOCK*N_LANES)-1 : 0]    o_data
+    output wire [(LEN_CODED_BLOCK*N_LANES)-1 : 0]   o_data
 );
 
     //Vector que almacena los tags de cada lane
-    reg         [N_LANES-1 : 0]                     am_insert_vector;
-    reg         [NB_CODED_BLOCK*N_LANES : 0]        out_data;
-    //reg         [NB_CODED_BLOCK*N_LANES-1 : 0]      data_vector;
+    wire        [(LEN_CODED_BLOCK*N_LANES)-1 : 0]     out_data;  
+        
 
     assign                                          o_data = out_data;
+
 
     genvar i;
     //generate
@@ -33,8 +33,7 @@ module am_insertion_toplevel
     begin :ger_block
         am_insertion
         #(
-            .NB_CODED_BLOCK     (NB_CODED_BLOCK),
-            .N_LANES            (N_LANES),
+            .LEN_CODED_BLOCK     (LEN_CODED_BLOCK),
             .AM_ENCODING_LOW    (AM_ENCODING_LOW),
             .AM_ENCODING_HIGH   (AM_ENCODING_HIGH),
             .NB_BIP             (NB_BIP)
@@ -43,10 +42,11 @@ module am_insertion_toplevel
         (
             .i_clock            (i_clock),
             .i_reset            (i_reset),
-            .i_enable           (i_enable),
-            .i_am_insert        (i_data[(NB_TAGGED_BLOCK*N_LANES)-1 - i*NB_TAGGED_BLOCK]),
-            .i_data             (i_data[(NB_CODED_BLOCK*N_LANES)-1 -: i*NB_CODED_BLOCK]),
-            .o_data             (out_data[(NB_CODED_BLOCK*N_LANES)-1 -: i*NB_CODED_BLOCK])
+            .i_enable           (i_enable && i_valid),
+            .i_valid            (i_valid),
+            .i_am_insert        (i_data[(LEN_TAGGED_BLOCK*N_LANES)-1 - i*LEN_TAGGED_BLOCK]),
+            .i_data             (i_data[(LEN_TAGGED_BLOCK*N_LANES)-2 -(i*LEN_TAGGED_BLOCK) -: LEN_CODED_BLOCK]),
+            .o_data             (out_data[((LEN_CODED_BLOCK*N_LANES)-1) -(i*LEN_CODED_BLOCK) -: LEN_CODED_BLOCK])
         );
         
     end
